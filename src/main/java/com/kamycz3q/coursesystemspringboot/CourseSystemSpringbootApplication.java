ab1fbedef17c1923c536c49b3de85647f795627d
Kamycz3q <overek.email@gmail.com>
2023-05-16 12:07:06 +0200

    Podstawowe pliki

---
+++ src/main/java/com/kamycz3q/coursesystemspringboot/CourseSystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kamycz3q.coursesystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSystemSpringbootApplication.class, args);
+	}
+
+}
