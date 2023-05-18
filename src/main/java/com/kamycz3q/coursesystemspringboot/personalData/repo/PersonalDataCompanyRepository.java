365d71eb7874c0021a8c43d202aacd878b27f1e4
Actiz77 <54171198+Kamycz3q@users.noreply.github.com>
2023-05-18 08:45:33 +0200

    Delete src/main/java/com/kamycz3q/coursesystemspringboot/personalData directory

--- src/main/java/com/kamycz3q/coursesystemspringboot/personalData/repo/PersonalDataCompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kamycz3q.coursesystemspringboot.personalData.repo;
-
-import com.kamycz3q.coursesystemspringboot.personalData.personalDataObject.PersonalDataCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonalDataCompanyRepository extends JpaRepository<PersonalDataCompany, Long> {
-}
